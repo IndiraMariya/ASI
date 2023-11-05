501168e7b5d49d5352e2e5d128083416c5ab1828
mariyind-mvla <100026909@mvla.net>
2023-11-04 18:09:47 -0700

    fixed load images path

--- src/load_images/load_latest.py
+++ src/load_images/load_latest.py
@@ -6,28 +6,27 @@ import os
 # download_path = sys.argv[1]
 
 # SDO WEBSITE URL
-latest_url = "https://sdo.gsfc.nasa.gov/assets/img/latest"
+latest_url = "https://sdo.gsfc.nasa.gov/assets/img/browse/2023"
 
 # DOWNLOAD PATH
-localdir = '/Users/indiram/ASI/imagefetch/images/images5'
+localdir = 'data/images'
 
 # "0094", "0131", "0171", "0193", "0211", "0304", "1600", "1700", 
 # "4096", "2048", "1024", 
 
-channels = ["0335", "HMIB", "HMII", "HMID", "HMIBC", "HMIIF", "HMIIC"]
-resolutions = ["512"]
+channels = ["0335", "HMIB", "HMIIC"]
+months = []
 
 for channel in channels:
-    for resoltion in resolutions:
-        FILENAME = f"latest_{resoltion}_{channel}.jpg"
-        URL = f"{latest_url}/{FILENAME}"
-        FILE_PATH = os.path.join(localdir, FILENAME)
+    FILENAME = f"latest_512_{channel}.jpg"
+    URL = f"{latest_url}/{FILENAME}"
+    FILE_PATH = os.path.join(localdir, FILENAME)
 
-        # Download the file
-        response = requests.get(URL)
-        if response.status_code == 200:
-            with open(FILE_PATH, "wb") as file:
-                file.write(response.content)
-            print(f"Downloaded: {FILE_PATH}")
-        else:
-            print(f"Failed to download: {URL}")
+    # Download the file
+    response = requests.get(URL)
+    if response.status_code == 200:
+        with open(FILE_PATH, "wb") as file:
+            file.write(response.content)
+        print(f"Downloaded: {FILE_PATH}")
+    else:
+        print(f"Failed to download: {URL}")
